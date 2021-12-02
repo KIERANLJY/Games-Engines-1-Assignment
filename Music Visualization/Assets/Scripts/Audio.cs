a6e57bf6094c53788fe591b536aaa92af6939c65
Your Name <you@example.com>
2021-12-02 18:43:40 +0000

    smoothing middle cubes

--- Music Visualization/Assets/Scripts/Audio.cs
+++ Music Visualization/Assets/Scripts/Audio.cs
@@ -8,6 +8,8 @@ public class Audio : MonoBehaviour
     AudioSource _audioSource;
     public static float[] _samples = new float[512];
     public static float[] _frequencyBands = new float[8];
+    public static float[] _bandsBuffer = new float[8];
+    float[] _bufferDecrease = new float[8];
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,7 @@ public class Audio : MonoBehaviour
     {
         GetSpectrumAudioSource();
         GetFrequencyBands();
+        GetBandsBuffer();
     }
 
     void GetSpectrumAudioSource()
@@ -46,6 +49,24 @@ public class Audio : MonoBehaviour
             }
             float _averageSpectrum = _sumSpectrum / _samplesInFB;
             _frequencyBands[i] = _averageSpectrum * 10;
+        }
+    }
+
+    void GetBandsBuffer()
+    {
+        for (int i = 0; i < 8; i ++)
+        {
+            if (_bandsBuffer[i] < _frequencyBands[i])
+            {
+                _bandsBuffer[i] = _frequencyBands[i];
+                _bufferDecrease[i] = 0.0005f;
+            }
+            if (_bandsBuffer[i] > _frequencyBands[i])
+            {
+                _bandsBuffer[i] -= _bufferDecrease[i];
+                _bufferDecrease[i] *= 1.5f;
+            }
+
         }
     }
 }
