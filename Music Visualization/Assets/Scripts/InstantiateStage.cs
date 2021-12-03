c435580df9ce93ab96168d5334e558c92f79b5df
Your Name <you@example.com>
2021-12-03 21:58:52 +0000

    stage instantiated

---
+++ Music Visualization/Assets/Scripts/InstantiateStage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateStage : MonoBehaviour
+{
+    public GameObject _stagePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject _instanceStage = (GameObject)Instantiate(_stagePrefab);
+        _instanceStage.transform.position = this.transform.position;
+        _instanceStage.transform.localScale = new Vector3(61, 5, 61);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
